e262c1ee1f8ee810b489c33b1b348aad0a248348
Peter Peck <petertpeck@gmail.com>
2024-03-10 22:54:40 -0600

    add local storage to todo list. Need to remove item from list and local storage next

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,52 @@ type Task = {
 }
 
 const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.getElementById("#new-task-form") as HTMLFormElement | null;
+const form = document.getElementById("new-task-form") as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks()
+
+tasks.forEach(addListItem)
 
 form?.addEventListener("submit", e => {
-e.preventDefault();
+	e.preventDefault();
 
-if(input?.value == "" || input?.value == null) return;
+	if(input?.value == "" || input?.value == null) return;
 
-const newTask: Task = {
-	id: uuidV4(),
-	title: input.value,
-	completed: false,
-	createdAt: new Date()
-}
+	const newTask: Task = {
+		id: uuidV4(),
+		title: input.value,
+		completed: false,
+		createdAt: new Date()
+	}
+	tasks.push(newTask)
 
 	addListItem(newTask)
- })
+	input.value = ""
+})
 
 function addListItem(task: Task){
+	const item = document.createElement("li");
+	const label = document.createElement("label")
+	const checkbox = document.createElement("input")
 
+	checkbox.addEventListener("change", () => {
+		task.completed = checkbox.checked
+		saveTasks()
+	})
+	checkbox.type = "checkbox"
+	checkbox.checked = task.completed;
+
+	label.append(checkbox, task.title)
+	item.append(label)
+	list?.append(item)
+}
+
+function saveTasks(){
+	localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(){
+	const taskJson = localStorage.getItem("TASKS")
+	if (taskJson == null) return  []
+	return JSON.parse(taskJson)
 }
